dd0f4396fa2d5e50011098956a8d4ea0e6396477
bjartur2004 <bjartur2004@gmail.com>
2025-11-24 13:34:09 +0000

    jioadfsijosadfijofads

--- Lota1/adferd_newtons.py
+++ Lota1/adferd_newtons.py
@@ -2,20 +2,21 @@ import matplotlib.pyplot as mpl
 import numpy as np
 
 def f(x):
-    return x**2-2
+    return 2*x**4 + 23*x**3 + 45*x**2 + 60*x + 50
+
 
 def df(x):
-    return 2*x
+    return 8*x**3 + 69*x**2 + 80*x + 60
 
 # plot
 x = np.linspace(-10,10,20)
 f_x = f(x)
 
 mpl.plot(x,f_x)
-mpl.show()
+#mpl.show()
 
-I_GUESS = 10
-TOLERANCE = 0.001
+I_GUESS = -9.41
+TOLERANCE = 0.00001
 
 def adferd_newtons(I_guess, tolerance):
     x = I_guess
@@ -24,7 +25,6 @@ def adferd_newtons(I_guess, tolerance):
     while abs(x-x_old)>tolerance:
         x_old = x
         x = x_old - f(x_old) / df(x_old)
-
     return x
 
 
