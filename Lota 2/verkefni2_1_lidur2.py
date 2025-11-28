539ee331fbc1fb6ac1422d8ce6fc61d4b767158b
thorvardure23@ru.is <thorvardure23@ru.is>
2025-11-28 15:38:27 +0000

    lota2_daemi2

--- Lota 2/verkefni2_1_lidur2.py
+++ Lota 2/verkefni2_1_lidur2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Mæld gögn
+# Gefin gögn
 t = np.array([1,3,5,7,9,11,13,15,17,19,21,23], dtype=float)
 q = np.array([12.0, 14.0, 13.9, 12.3, 11.7, 8.9, 6.3, 6.5, 6.1, 5.8, 6.6, 9.3], dtype=float)
 
@@ -14,7 +14,7 @@ M = np.column_stack([
     np.ones_like(t)
 ])
 
-
+# komum ofákvörðuðu fylki yfir í ferhyrnt kerfi
 x, _, _, _= np.linalg.lstsq(M, q, rcond=None)
 A, B, C = x
 
