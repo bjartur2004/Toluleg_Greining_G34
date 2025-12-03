3f4574dea60ddf950450df7e10e38e13c8718f81
thorvardure23@ru.is <thorvardure23@ru.is>
2025-12-03 12:24:45 +0000

    j

---
+++ Lota 3/Euler_kerfi.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from Euler_step import eulerstep   # undirforrit 1
+from ydot import ydot
+
+def pendulum_euler(theta0, dtheta0, T, n):
+    
+    h = T / n
+    t = np.linspace(0.0, T, n + 1)
+
+    Y = np.zeros((n + 1, 2))
+    Y[0, 0] = theta0
+    Y[0, 1] = dtheta0
+
+    for k in range(n):
+        Y[k + 1, :] = eulerstep(t[k], Y[k, :], h)
+
+    return t, Y
+
