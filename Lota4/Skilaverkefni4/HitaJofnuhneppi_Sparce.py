1ebf3ccd1fba993e28b6a26c1149f5d9d09e1010
bjartur2004 <bjartur2004@gmail.com>
2025-12-11 13:52:56 +0000

    fallegt sparce version

---
+++ Lota4/Skilaverkefni4/HitaJofnuhneppi_Sparce.py
@@ -0,0 +1,91 @@
+import numpy as np
+import scipy as sp
+from math import ceil
+
+def A(n,m,arguments):
+    P, L, K, H, delta, Power_L, ambient_tempeature = arguments
+    h = L/(n-1)
+
+    entries = [[],[],[]] # shape: [[v0,v1,v2], [I0,I1,I2], [J0,J1,J2]]
+    def addEntry(value,I,J):
+        entries[0].append(value)
+        entries[1].append(I)
+        entries[2].append(J)
+
+    # neðri hlið
+    j = 0
+    for i in range(1, m-1):
+        k = i + j*m
+        addEntry(-3 + (2*h*H)/K, k, k)
+        addEntry(4,              k, k+m)
+        addEntry(-1,             k, k+2*m)
+
+
+    # efri hlið
+    j = n-1
+    for i in range(1, m-1):
+        k = i + j*m
+        addEntry( 3 - (2*h*H)/K, k, k)
+        addEntry(-4,             k, k-m)
+        addEntry( 1,             k, k-2*m)
+
+    
+    # hægri hlið
+    i = m-1
+    for j in range(n):
+        k = i + j*m
+        addEntry( 3 - (2*h*H)/K, k, k)
+        addEntry(-4,             k, k-1)
+        addEntry( 1,             k, k-2)
+
+
+    # vinstri efri hlið
+    i = 0
+    for j in range(ceil(n*(Power_L/L)), n):
+        k = i + j*m
+        addEntry( 3 - (2*h*H)/K, k, k)
+        addEntry(-4,             k, k+1)
+        addEntry( 1,             k, k+2)
+
+
+    # vinstri neðri hlið
+    i = 0
+    for j in range(0, ceil(n*(Power_L/L))):
+        k = i + j*m
+        addEntry(-3, k, k)
+        addEntry( 4, k, k+1)
+        addEntry(-1, k, k+2)
+
+    # innri stök
+    for i in range(1, m-1):
+        for j in range(1, n-1):
+            k = i + j*m
+            addEntry(-4 - (2*H*h**2)/(K*delta), k, k)
+            addEntry(1, k, k-1)
+            addEntry(1, k, k+1)
+            addEntry(1, k, k-m)
+            addEntry(1, k, k+m)
+    
+    A = sp.sparse.csr_matrix((entries[0], (entries[1], entries[2])), shape=(n*m, n*m))
+    return A
+
+
+def b(n,m, arguments):
+    P, L, K, H, delta, Power_L, ambient_tempeature = arguments
+
+    b = np.zeros([n*m])
+    h = L/(n-1)
+
+    i = 0
+    for j in range(ceil(n*(Power_L/L))):
+        k = i+j*m
+        b[k] = -2*h*P/(L*delta*K)
+    return b
+
+
+def solve_u(n,m,arguments): # argumens should be [P,L,K,H,Delta,Power_L, ambient_tempeature]
+    P, L, K, H, delta, Power_L, ambient_tempeature = arguments
+    u = sp.sparse.linalg.spsolve(A(n,m, arguments), b(n,m, arguments))
+    u_corrected = np.array(u) + 20 
+
+    return u_corrected
--- Lota4/Skilaverkefni4/S4_sparseLausnHitaDreifing.py
+++ Lota4/Skilaverkefni4/S4_sparseLausnHitaDreifing.py
@@ -1,5 +1,5 @@
 import HitadreifingsPlot
-import HitaJofnuhneppi_full
+import HitaJofnuhneppi_Sparce
 
 P = 5
 L = 2 #cm
@@ -11,10 +11,10 @@ ambient_tempeature = 20 #°C
 
 arguments = [P,L,K,H,delta,Power_L,ambient_tempeature]
 
-n = 10
-m = 10
-u = HitaJofnuhneppi_full.solve_u(n,m,arguments)
+n = 100
+m = 100
+u = HitaJofnuhneppi_Sparce.solve_u(n,m,arguments)
 
 print(f"u max: {max(u):.4f}")
 
-HitadreifingsPlot.plotHitadreifingu(u,n,m, arguments, "Hitadreifing með n=m=10")
+HitadreifingsPlot.plotHitadreifingu(u,n,m, arguments, "Hitadreifing Leist með Rýrs fylkja aðferð")
